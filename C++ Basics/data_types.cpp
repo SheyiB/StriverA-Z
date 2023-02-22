17bedf4698fa24fa99d080407922e269e8513951
Seyi-Banjo <elijahbanjo@gmail.com>
2023-02-22 17:04:33 +0100

    Basic Data types

---
+++ C++ Basics/data_types.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//First Comment
+
+int main(){
+    // int
+    int a = 10;
+    //long
+    long b = 15;
+    // long long
+    long long c = 1500000000000000;
+    //double
+    double d = 5.67;
+    //float
+    float f = 6.9;
+    //string uses double quote
+    string e = "Elijah";
+    //getline
+    string g;
+    cout << "Enter a Statement: ";
+    getline( cin, g);
+    cout << "You said : "<<g; 
+    //char - using single quote
+    char hh;
+    cin >> hh;
+    cout << hh;
+
+
+    return 0;
+
+
+}
