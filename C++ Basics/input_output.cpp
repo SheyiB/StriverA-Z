f12baf573224ec197b3061087cf44be4802042f1
Seyi-Banjo <elijahbanjo@gmail.com>
2023-02-22 16:38:05 +0100

    Striver A-Z

---
+++ C++ Basics/input_output.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+ int main() {
+    cout << "Hey Banjo" << endl <<"Today is Fave's Birthday" ;
+    cout << "\n" << "I love her" << endl ;
+
+    int x, y;
+    
+    cout << "Enter The value of X:";
+    cin >> x;
+    cout << "Enter The value of Y:" ;
+    cin >> y;
+    cout << "Value of X is : " << x << " and the value of Y is " << y;
+
+    return 0;
+ }
