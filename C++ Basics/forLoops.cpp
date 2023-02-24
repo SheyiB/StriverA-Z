2425bcd6554a028b6870e217fe11a4cd8b24f90f
Seyi-Banjo <elijahbanjo@gmail.com>
2023-02-24 15:47:56 +0100

    For Loops

---
+++ C++ Basics/forLoops.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    //Counter to see how many times out loop runs
+    int x = 1;
+    for(float i = 0; i < 10; i = i + 0.5){
+
+        cout << "  C++ is amazing | " << "Count " << x  << endl ;
+        //increament counter
+        x = x+1;
+    }
+    return 0;
+}
