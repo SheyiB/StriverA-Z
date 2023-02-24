03dda61bba20a6329f537ede70a90dc07a5d9aec
Seyi-Banjo <elijahbanjo@gmail.com>
2023-02-24 15:56:54 +0100

    While loops

---
+++ C++ Basics/whileLoops.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int x = 10;
+    while(x > 0){
+        cout << "Elijah is a genius " << x << endl;
+        x = x -1;
+    }
+
+
+    //Do while Loop, allows you to perform an action for a least the first time before checking the condition for it to perform
+    do {
+        cout << "Google! Elijah is coming";
+    }
+    while(x > 10);
+
+    return 0;
+}
