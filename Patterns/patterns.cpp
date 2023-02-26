81c0164c2e9545f4eb1deff72d70e4372809773c
Seyi-Banjo <elijahbanjo@gmail.com>
2023-02-26 22:33:24 +0100

    Pattern 7-10

--- Patterns/patterns.cpp
+++ Patterns/patterns.cpp
@@ -60,6 +60,89 @@ void pattern6(int n){
         cout << endl;
     }
 }
+
+void pattern7(int n){
+    for(int i = 0; i < n; i++){
+        for (int j = 0; j < n-i-1; j++ ){
+            cout << " ";
+        }
+        for (int j = 0; j < 2*i + 1; j++ ){
+            cout << "*";
+        }
+        for (int j = 0; j < n-i-1; j++ ){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern8(int n){
+    for(int i = n-1; i > -1; i--){
+        for (int j = 0; j < n-i-1; j++ ){
+            cout << " ";
+        }
+        for (int j = 0; j < 2*i + 1; j++ ){
+            cout << "*";
+        }
+        for (int j = 0; j < n-i-1; j++ ){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern9(int n){
+
+       for(int i = 0; i < n; i++){
+        for (int j = 0; j < n-i-1; j++ ){
+            cout << " ";
+        }
+        for (int j = 0; j < 2*i + 1; j++ ){
+            cout << "*";
+        }
+        for (int j = 0; j < n-i-1; j++ ){
+            cout << " ";
+        }
+        cout << endl;
+    }
+
+    for(int i = n-1; i > -1; i--){
+        for (int j = 0; j < n-i-1; j++ ){
+            cout << " ";
+        }
+        for (int j = 0; j < 2*i + 1; j++ ){
+            cout << "*";
+        }
+        for (int j = 0; j < n-i-1; j++ ){
+            cout << " ";
+        }
+        cout << endl;
+    }   
+}
+
+void pattern10(int n){
+    int x = 1;
+    for(int i = 1; i<2*n; i++){
+        
+        if (i < n){
+            for(int j = 0; j< x; j++ ){
+            cout << "* ";
+            }
+            x++;
+                
+        }
+        else{
+            for(int j = x; j > 0 ; j-- ){
+            cout << "* ";
+            }
+            x--;    
+        }
+
+        cout << endl;
+
+        
+    }
+}
 int main(){
 
     //pattern1(5);
@@ -67,7 +150,11 @@ int main(){
     //pattern3(5);
     //pattern4(5);
     //pattern5(5);
-    pattern6(5);
+    //pattern6(5);
+    //pattern7(5);
+    //pattern8(5);
+    //pattern9(5);
+    pattern10(5);
 
     return 0;
 }
