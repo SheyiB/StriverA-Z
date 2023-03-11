64f898ef51b5f6b574a02c3b30915cec51f593a1
Seyi-Banjo <elijahbanjo@gmail.com>
2023-03-11 23:02:17 +0100

    Pairs STL Update

--- C++STL/pairs.cpp
+++ C++STL/pairs.cpp
@@ -5,20 +5,20 @@ using namespace std;
 int main(){
     pair <int, int > p = {3,4};
 
-    cout << "The number pairs are" << p.first << "&" << p.second << endl;
+    cout << "The number pairs are " << p.first << " & " << p.second << endl;
 
     pair <string, string> couple = {"Ronaldo", "Georgina"};
     
-    cout << "The Couples are" << couple.first << "&" << couple.second << endl;
+    cout << "The Couples are " << couple.first << " & " << couple.second << endl;
 
 
     pair <string , pair < string, int> > person = {"Talms", {"France", 28}};
         
-    cout << "His name is" << person.first << "and he lives in" << person.second.first << " and he is " << person.second.second << "years old" << endl;
+    cout << "His name is " << person.first << "and he lives in " << person.second.first << " and he is " << person.second.second << "years old" << endl;
 
     pair <string, int> studentList [] = { {"Tide", 23}, {"Suli", 20}};
 
-    cout << "The second student in the list is " << studentList[1].first << " and she is " << studentList[1].second << " years old";
+    cout << "The second student in the list is " << studentList[1].first << " and she is " << studentList[1].second << "years old";
 
     return 0;
 }
