4560740e156bf73f87f47c53ee5854e26004b5ce
Seyi-Banjo <elijahbanjo@gmail.com>
2023-03-18 23:12:32 +0100

    Pattern Ten Function

--- Patterns/patterns.py
+++ Patterns/patterns.py
@@ -69,11 +69,12 @@ def pattern_nine():
         spaces = p - i
         print(' '*spaces + '*' * stars + ''*spaces)
 
-n=5
-for i in range(1,n+1):
-    print("*" * i)
-for i in range(n-1,0,-1):
-    print("*" * i)
+def pattern_ten():
+    n=5
+    for i in range(1,n+1):
+        print("*" * i)
+    for i in range(n-1,0,-1):
+        print("*" * i)
 
 n=5
 for i in range(1,n+1):
