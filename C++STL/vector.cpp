62273318562febf0c796e00b0ff281c8e1f4e44f
Seyi-Banjo <elijahbanjo@gmail.com>
2023-03-21 15:28:39 +0100

    Iterators

--- C++STL/vector.cpp
+++ C++STL/vector.cpp
@@ -11,22 +11,46 @@ int main(){
     myVector.push_back(1);
     // For adding element to vector but faster
     myVector.emplace_back(2);
+    myVector.emplace_back(31);
+    myVector.emplace_back(29);
+    myVector.emplace_back(12);
+    myVector.emplace_back(23);
+    myVector.emplace_back(40);
+    myVector.emplace_back(12);
+    myVector.emplace_back(30);
+    myVector.emplace_back(20);
+    myVector.emplace_back(9);
+    
+
 
     // Creating a vector pair
     vector <pair <int, int>> myPairVector ;
 
     //When using Push back to add to vector you need specify item properly
-    myPairVector.push_back({4,5});
+    myPairVector.push_back({5,4});
+    myPairVector.push_back({6,3});
+    myPairVector.push_back({7,2});
+    myPairVector.push_back({8,1});
+    myPairVector.push_back({9,0});
 
     //When using emplace back, no need to specify
     myPairVector.emplace_back(7,9);
 
-    vector<string> stringVec('Shola');
+    vector<string> stringVec('S');
 
+    stringVec.emplace_back('J', 'K');
+    
     vector<int> k(5,10);
 
-
     vector<int> h(k);
+
+    cout << myVector.at(0) << " This guy " << stringVec[0]  << endl;
+
+    //Iterators
+
+    vector<int>::iterator myit = myVector.begin();
+
+    cout << "My Lorde" << *(myit) << endl;
 
     return 0;
 }
